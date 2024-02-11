b5bd2e7c4ae574151c2ed9a1ecbbba029ae0774f
trippleflipp <87667497+trippleflipp@users.noreply.github.com>
2024-02-11 22:02:45 +0700

    Create keyboards.py
    keyboards added

---
+++ src/sfu_telegram_bot/bot/app/keyboards.py
@@ -0,0 +1,12 @@
+from aiogram.types import ReplyKeyboardMarkup
+from emoji import emojize
+
+menu_board = ReplyKeyboardMarkup(resize_keyboard=True)
+menu_board.row(emojize('Что сегодня? :student:'), emojize('Расписание :teacher:'))
+menu_board.row(emojize('Отметиться на физру :person_cartwheeling:'))
+menu_board.row(emojize('Авторизоваться :rocket:'))
+
+timetable_board = ReplyKeyboardMarkup(resize_keyboard=True)
+timetable_board.row('Эта неделя')
+timetable_board.row('Четная неделя', 'Нечетная неделя')
+timetable_board.row('Назад')
